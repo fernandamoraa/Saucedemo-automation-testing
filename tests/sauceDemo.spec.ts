63f867a9325f883dc8895d8093a1678191170867
fernandamoraa <fermora2509@gmail.com>
2026-06-25 11:19:57 -0600

    SCRUM-25 Automate Test Case

--- tests/sauceDemo.spec.ts
+++ tests/sauceDemo.spec.ts
@@ -5,7 +5,7 @@ test('TC 01: Loging with Valid Standard User', async ({ page }) => {
     await page.locator('[data-test="username"]').fill('standard_user');
     await page.locator('[data-test="password"]').fill('secret_sauce');
     await page.locator('[data-test="login-button"]').click();
-    await expect.soft(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
     await expect(page.getByText('Swag Labs')).toBeVisible();
 
 });
@@ -16,4 +16,16 @@ test('TC 02: Error Handling for Locked Out User', async ({ page }) => {
     await page.locator('[data-test="password"]').fill('secret_sauce');
     await page.locator('[data-test="login-button"]').click();
     await expect(page.getByText('Epic sadface: Sorry, this user has been locked out.')).toBeVisible();
+});
+
+test('TC 03: Secure Logout Flow', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.locator('[data-test="username"]').fill('standard_user');
+    await page.locator('[data-test="password"]').fill('secret_sauce');
+    await page.locator('[data-test="login-button"]').click();
+    await page.locator('[id="react-burger-menu-btn"]').click(); 
+    await page.locator('[data-test="logout-sidebar-link"]').click();
+    await expect(page).toHaveURL('https://www.saucedemo.com/')
+    await page.goto('https://www.saucedemo.com/inventory.html');
+    await expect(page.getByText("Epic sadface: You can only access '/inventory.html' when you are logged in.")).toBeVisible();
 });
