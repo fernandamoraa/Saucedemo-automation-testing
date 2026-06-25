ea90f577fe14d7723d5cd0276f5f401d2b734b45
fernandamoraa <fermora2509@gmail.com>
2026-06-25 10:27:16 -0600

    SCRUM-20 Automate Test Case

--- tests/sauceDemo.spec.ts
+++ tests/sauceDemo.spec.ts
@@ -10,3 +10,10 @@ test('TC 01: Loging with Valid Standard User', async ({ page }) => {
 
 });
 
+test('TC 02: Error Handling for Locked Out User', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.locator('[data-test="username"]').fill('locked_out_user');
+    await page.locator('[data-test="password"]').fill('secret_sauce');
+    await page.locator('[data-test="login-button"]').click();
+    await expect(page.getByText('Epic sadface: Sorry, this user has been locked out.')).toBeVisible();
+});
