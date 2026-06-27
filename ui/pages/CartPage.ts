91287986e75d3ad4b56750a05d0e07d19b36ff6a
fernandamoraa <fermora2509@gmail.com>
2026-06-27 15:21:14 -0600

    Pages

---
+++ ui/pages/CartPage.ts
@@ -0,0 +1,13 @@
+import { Locator,Page}from '@playwright/test';
+
+export class CartPage {
+    public readonly checkoutButton: Locator;
+
+    constructor(page:Page){
+        this.checkoutButton = page.locator('[data-test="checkout"]');
+    }
+
+    async startCheckout(){
+await this.checkoutButton.click();
+    }
+}
--- ui/pages/InventoryPage.ts
+++ ui/pages/InventoryPage.ts
@@ -3,14 +3,27 @@ import {Locator, Page}from '@playwright/test';
 export class InventoryPage {
     public readonly hamburguerButton: Locator;
     public readonly logoutButton: Locator;
+    public readonly addToCartButton: Locator;
+    public readonly cartIcon: Locator;
 
-    constructor (page:Page){
+    constructor(page:Page) {
         this.hamburguerButton = page.locator('[id="react-burger-menu-btn"]');
         this.logoutButton = page.locator('[data-test="logout-sidebar-link"]');
+        this.addToCartButton = page.locator('[id="add-to-cart-sauce-labs-backpack"]');
+        this.cartIcon = page.locator ('[data-test="shopping-cart-badge"]');
+        this.cartIcon = page.locator('[data-test="shopping-cart-link"]');
     }
 
-async logout (){
+async logout(){
     await this.hamburguerButton.click();
-    await this.logoutButton.click();
+    await this.logoutButton.click();   
+}
+
+async addToCart(){
+    await this.addToCartButton.click();
+}
+
+async openCart(){
+    await this.cartIcon.click();
 }
 }
